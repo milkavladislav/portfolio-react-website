1fa25495c4c7676ad26b56dc4c674f0c472a6ad6
milkavladislav <milkav06062003@gmail.com>
2022-12-24 20:38:13 +0200

    add portfolio section

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ import Footer from "./components/footer/Footer";
 import Header from "./components/header/Header";
 import Nav from "./components/nav/Nav";
 import Portfolio from "./components/portfolio/Portfolio";
-import Services from "./components/services/Services";
 import Testimonials from "./components/testimonials/Testimonials";
 
 function App() {
@@ -16,7 +15,6 @@ function App() {
       <Nav />
       <About />
       <Experience />
-      <Services />
       <Portfolio />
       <Testimonials />
       <Contact />
--- src/components/portfolio/Portfolio.tsx
+++ src/components/portfolio/Portfolio.tsx
@@ -1,8 +1,49 @@
 import React from "react";
 import "./Portfolio.css";
+import SectionWrapper from "../common/SectionWrapper";
+import "./Portfolio.css";
+import IMG1 from "../../assets/portfolio1.jpg";
+import IMG2 from "../../assets/portfolio2.jpg";
+import IMG3 from "../../assets/portfolio3.jpg";
+import IMG4 from "../../assets/portfolio4.jpg";
+import IMG5 from "../../assets/portfolio5.png";
+import IMG6 from "../../assets/portfolio6.jpg";
+
+const portfolioImage = [IMG1, IMG2, IMG3, IMG4, IMG5, IMG6];
 
 const Portfolio = () => {
-  return <section id="portfolio">Portfolio</section>;
+  return (
+    <SectionWrapper
+      subtitle={"My Recent Work"}
+      title={"Portfolio"}
+      containerClass={"portfolio__container"}
+      id={"portfolio"}
+    >
+      <>
+        {portfolioImage.map((image) => (
+          <article className="portfolio__item">
+            <div className="portfolio__item-image">
+              <img src={image} alt="portfolio1" />
+            </div>
+            <h3>This is a portfolio item title</h3>
+            <div className="portfolio__item-cta">
+              <a href="http://github.com" className="btn">
+                GitHub
+              </a>
+              <a
+                href="http://github.com"
+                className="btn btn-primary"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Live Demo
+              </a>
+            </div>
+          </article>
+        ))}
+      </>
+    </SectionWrapper>
+  );
 };
 
 export default Portfolio;
